590a39c5acae9d32d3be10da637af0affbe467a9
= <admin@selecton.net>
2011-01-30 20:19:10 +0100

    Dodalem view helper ShortenTekst
    Ten View helper obcina ciag znakow do podanej ilosci. Nie ucina w polowie slowa. Helper cofa sie tak dlugo az napotka spacje i ucina w miejscu spacji.

---
+++ Sel/View/Helper/ShortenText.php
@@ -0,0 +1,27 @@
+<?php
+namespace Sel\View\Helper;
+
+use Sel\View\HelperAbstract;
+
+/**
+ * ShortenTekst
+ *
+ * @author Szymon Wygnański
+ * @license http://creativecommons.org/licenses/by/3.0/pl/
+ */
+class ShortenText extends HelperAbstract
+{
+
+    public function direct($text, $length)
+    {
+        
+        if( $length >= \strlen($text) )
+            return $text;
+
+        while( $text[$length] != ' ' || $length == 0)
+            $length--;
+
+        return \substr($text, 0, $length) . '...';
+    }
+
+}
