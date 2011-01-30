b55d0ff1f0ea92ae546f4f75e16fb7a99fda63f2
= <admin@selecton.net>
2011-01-30 13:38:42 +0100

    Dodalem view helper Partial

---
+++ Sel/View/Helper/Partial.php
@@ -0,0 +1,27 @@
+<?php
+namespace Sel\View\Helper;
+
+use Sel\View\HelperAbstract;
+use Sel\View;
+
+/**
+ * Partial
+ *
+ * @author Szymon Wygnański
+ * @license http://creativecommons.org/licenses/by/3.0/pl/
+ */
+class Partial extends HelperAbstract
+{
+
+    public function direct($script, $params = array())
+    {
+        $view = new View();
+        foreach($params as $key=>$val)
+        {
+            $view->$key = $val;
+        }
+        $view->set_script($script);
+        return $view->render();
+    }
+
+}
